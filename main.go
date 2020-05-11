8b5f65e130bc98427836afe3e0682eee9aca687d
abdul rasyid <zerone0101@yahoo.co.id>
2020-05-11 23:02:01 +0700

    logic antrian loket

--- main.go
+++ main.go
@@ -1,15 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type person struct {
+	id       int
+	duration time.Duration
+}
+
+func worker(id int, loket chan person, done chan bool) {
+	for l := range loket {
+		log.Println("loket", id, "started job for person ", l.id)
+		time.Sleep(l.duration)
+		log.Println("loket", id, "finished job for person", l.id, "time process:",l.duration.Seconds(),"second")
+		done <- true
+	}
+}
 
 func main()  {
-	//duration := []int{1, 1, 5, 1, 1}
-	//var persons []person
+	duration := []int{1, 1, 5, 1, 1}
+	var persons []person
 	var jmlLoket int
 
-	fmt.Print("Masukkan jumlah loket:")
+	log.Println("Masukkan jumlah loket:")
 	_, err := fmt.Scanf("%d", &jmlLoket)
 	if err != nil {
-		fmt.Println("Error Input :", err.Error())
+		log.Println("Error Input :", err.Error())
+	}
+
+	for i, d := range duration {
+		persons = append(persons, person{
+			id: i + 1, duration: time.Duration(d) * time.Second,
+		})
+	}
+
+	numJobs := len(persons)
+	loket := make(chan person, numJobs)
+	done := make(chan bool, numJobs)
+
+	// jumlah loket yg beroperasi
+	for w := 1; w <= jmlLoket; w++ {
+		go worker(w, loket, done)
+	}
+
+	for _, o := range persons {
+		loket <- o
+	}
+	close(loket)
+
+	for j := 1; j <= numJobs; j++ {
+		<-done
 	}
 }
